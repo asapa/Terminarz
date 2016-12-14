21fa7c9bc14fbf48eb1e404fc920e032bfbbd0df
Artur <asafa.urban@gmail.com>
2016-12-14 13:24:39 +0100

    Layout logowania(portrait/land), shared preferences(login), aktywności AddTask i EditTask wraz z layoutami(portrait/land)

--- app/src/main/java/com/example/asapa/terminarz/MainActivity.java
+++ app/src/main/java/com/example/asapa/terminarz/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.asapa.terminarz;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText loginEditText,passEditText;
+    Button btnLogin;
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginEditText = (EditText) findViewById(R.id.loginEditText);
+        passEditText = (EditText) findViewById(R.id.passEditText);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        loadPreferences();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savePreferences();
+                Intent intent = new Intent(MainActivity.this,ShowTask.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void loadPreferences(){
+        SharedPreferences mp = getSharedPreferences("MyPreference", Context.MODE_PRIVATE);
+        loginEditText.setText(mp.getString("text",""));
+
+    }
+
+    public void savePreferences(){
+        SharedPreferences mp = getSharedPreferences("MyPreference", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mp.edit();
+        String text = loginEditText.getText().toString();
+        editor.putString("text",text);
+        editor.commit();
     }
 }
